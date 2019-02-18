ce498e4c50cdd2bd8eec94fc66533ea53f40df9c
GGR-JPL <no-reply@example.org>
2019-02-18 13:04:00 -0500

    Complétion de la classe Client

--- TP1-2/src/Client.java
+++ TP1-2/src/Client.java
@@ -1,4 +1,18 @@
 
 public class Client {
-
+	
+	private String nom;
+	
+	public Client(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
 }
