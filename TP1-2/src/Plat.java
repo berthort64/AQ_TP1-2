66c35a7ad0b6dccc38a77e68b4fd55afbd7a65af
Misterjo <plarocque.joel@gmail.com>
2019-02-18 13:50:08 -0500

    Completion de la classe Plat

--- TP1-2/src/Plat.java
+++ TP1-2/src/Plat.java
@@ -1,4 +1,27 @@
-
 public class Plat {
+
+private String produit;
+private double prix;
+
+	public Plat(String produit,double prix){
+	
+	this.produit=produit;
+	this.prix=prix;
+	
+	}
+
+
+	public String getProduit(){
+
+	return this.produit;
+	
+	}
+	
+	public double getPrix(){
+		
+	return this.prix;
+	
+	}
+
 
 }
