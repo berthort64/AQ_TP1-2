3c9e373d38574ff7ce368214b0aa976540168bea
GGR-JPL <no-reply@example.org>
2019-03-20 17:43:49 -0400

    Correction de la lecture du fichier

--- TP1-2/src/LectureFichier.java
+++ TP1-2/src/LectureFichier.java
@@ -8,6 +8,8 @@ public class LectureFichier {
 	private static ArrayList<Commande> commandes 	= new ArrayList<Commande>();
 	private static ArrayList<Plat> plats 			= new ArrayList<Plat>();
 	
+	private static int etat = 0;
+	
 	public static void main (String[] args) throws IOException {
 		
 		//Nom des fichiers
@@ -32,73 +34,104 @@ public class LectureFichier {
 		
 		while ((ligne = br.readLine()) != null) {
 			
-			//On ignore les lignes servant au repérage
-			if (ligne != "Clients :" && ligne != "Plats :" && ligne != "Commandes :" && ligne != "Fin") {
+			switch(ligne) {
+			case "Clients :":
+				etat = 1;
+				break;
+				
+			case "Plats :":
+				etat = 2;
+				break;
+				
+			case "Commandes :":
+				etat = 3;
+				break;
+				
+			case "Fin :":
+				etat = -1;
+				break;
+				
+			default:
 				
 				String[] contenu = ligne.split(" ");
 				
-				switch (contenu.length) {
+				switch(etat) {
 				case 1:
 					
-					Client client = new Client(contenu[0]);
-					clients.add(client);
-					
+					if (contenu.length == 1) {
+						Client client = new Client(contenu[0]);
+						clients.add(client);
+					} else {
+						System.out.println('"' + ligne + "\" : Le client ne respecte pas le format demandé\nLe client n'a pas été ajouté.\n");
+					}
 					break;
+					
 				case 2:
 					
-					Plat plat = new Plat(contenu[0], Double.parseDouble(contenu[1]));
-					plats.add(plat);
-					
+					if (contenu.length == 2) {
+						Plat plat = new Plat(contenu[0], Double.parseDouble(contenu[1]));
+						plats.add(plat);
+					} else {
+						System.out.println('"' + ligne + "\" : Le plat ne respecte pas le format demandé\nLe plat n'a pas été ajouté.\n");
+					}
 					break;
+					
 				case 3:
 					
-					String nomClient = contenu[0];
-					String nomProduit = contenu[1];
-					int qte = Integer.parseInt(contenu[2]);
-					
-					Client clientCommande = null;
-					Plat platCommande = null;
-					
-					for (Client c : clients) {
-						if (c.getNom().equals(nomClient)) {
-							clientCommande = c;
-							break;
+					if (contenu.length == 3) {
+						String nomClient = contenu[0];
+						String nomProduit = contenu[1];
+						int qte = Integer.parseInt(contenu[2]);
+						
+						Client clientCommande = null;
+						Plat platCommande = null;
+						
+						for (Client c : clients) {
+							if (c.getNom().equals(nomClient)) {
+								clientCommande = c;
+								break;
+							}
 						}
-					}
-					
-					if (clientCommande == null) {
 						
-						System.out.println('"' + ligne + "\" : Le client \"" + nomClient + "\" n'existe pas\nLa commande n'a pas été ajoutée.\n");
-						break;
-						
-					}
-					
-					for (Plat p : plats) {
-						if (p.getProduit().equals(nomProduit)) {
-							platCommande = p;
+						if (clientCommande == null) {
+							
+							System.out.println('"' + ligne + "\" : Le client \"" + nomClient + "\" n'existe pas\nLa commande n'a pas été ajoutée.\n");
 							break;
+							
 						}
-					}
-					
-					if (platCommande == null) {
 						
-						System.out.println('"' + ligne + "\" : Le plat \"" + nomProduit + "\" n'existe pas\nLa commande n'a pas été ajoutée.\n");
-						break;
+						for (Plat p : plats) {
+							if (p.getProduit().equals(nomProduit)) {
+								platCommande = p;
+								break;
+							}
+						}
 						
+						if (platCommande == null) {
+							
+							System.out.println('"' + ligne + "\" : Le plat \"" + nomProduit + "\" n'existe pas\nLa commande n'a pas été ajoutée.\n");
+							break;
+							
+						}
+						
+						Commande commande = new Commande(clientCommande, platCommande, qte);
+						commandes.add(commande);
+						
+						clientCommande.ajouterCommande(qte * platCommande.getPrix());
+					} else {
+						System.out.println('"' + ligne + "\" : La commande ne respecte pas le format demandé\nLa commande n'a pas été ajoutée.\n");
 					}
-					
-					Commande commande = new Commande(clientCommande, platCommande, qte);
-					commandes.add(commande);
-					
-					clientCommande.ajouterCommande(qte * platCommande.getPrix());
-					
 					break;
-				default:
-					System.out.println("Le fichier ne respecte pas le format demandé");
+					
+				// Cas d'erreur
+				case 0:
+					System.out.println('"' + ligne + "\" : Le fichier ne respecte pas le format demandé : Doit commencer par la section 'Clients'.\nLa ligne a été ignorée.\n");
+					break;
+				case -1:
+					System.out.println('"' + ligne + "\" : Le fichier ne respecte pas le format demandé : Aucune ligne ne doit suivre la ligne 'Fin'.\nLa ligne a été ignorée.\n");
+					break;
 				}
-				
 			}
-			
 		}
 		
 		br.close();
